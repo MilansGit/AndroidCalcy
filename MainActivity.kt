9c5ad8991ea6c54dc0736fade5e1aa1ad1555ed9
Milan <115487954+MilansGit@users.noreply.github.com>
2023-08-25 22:20:57 +0530

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calcy
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,14 +17,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    @SuppressLint("SuspiciousIndentation")
     fun numberAction(view: View){
+
         if(view is Button){
-            if(view.text)
+            if(view.text == "."){
+                if(canAddDecimal)
+                    workingsTV.append(view.text)
+
+                canAddDecimal = false
+            }
+            else
             workingsTV.append(view.text)
             canAddOperation = true
         }
     }
-    fun oprationaction(view: View){
+    fun oprationAction(view: View){
         if(view is Button && canAddOperation){
             workingsTV.append(view.text)
             canAddOperation = false
@@ -41,5 +50,96 @@ class MainActivity : AppCompatActivity() {
             workingsTV.text = workingsTV.text.subSequence(0,length-1)
         }
     }
-    fun equalsAction(view: View) {}
+    fun equalsAction(view: View) {
+        resultsTV.text = calculateResults()
+    }
+
+    private fun calculateResults(): String
+    {
+        val digitsOperators = digitsoperator()
+        if (digitsOperators.isEmpty()) return ""
+
+        val timesDivision = timesDivisionCalculate(digitsOperators)
+        if (timesDivision.isEmpty()) return ""
+
+        val result = addSubtractCalculate(timesDivision)
+
+        return Float.toString()
+    }
+
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Float
+    {
+        var result
+    }
+
+    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any> {
+
+        var list = passedList
+        while (list.contains('x') || list.contains('/') ){
+            list = calcTimesDiv(list)
+        }
+        return list
+    }
+
+    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any>
+    {
+
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size
+
+        for(i in passedList.indices)
+        {
+
+            if(passedList[i] is Char && i !== passedList.LastIndex && i < restartIndex )
+            {
+                val operator = passedList[i]
+                val prevDigit = passedList[i] as Float
+                val nextDigit = passedList[i] as Float
+                when(operator)
+                {
+                    'x' ->
+                    {
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' ->
+                    {
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+                    else ->
+                    {
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+
+            if(i > restartIndex)
+                newList.add(passedList[i])
+        }
+        return newList
+
+    }
+
+
+    private fun digitsoperator(): MutableList<Any>
+    {
+        val list = mutableListOf<Any>()
+        var currentDigit= ""
+        for(charactor in workingsTV.text){
+            if(charactor.isDigit() || charactor == '.')
+                currentDigit += charactor
+            else{
+                list.add(currentDigit.toFloat())
+                currentDigit=""
+                list.add(charactor)
+            }
+        }
+        if(currentDigit != "")
+            list.add(currentDigit.toFloat())
+
+        return list
+        }
+    }
 }
